7cc8b2779ace74071527bfff03356494ca44e86e
Andrey Karpov <a_karp@mail.ru>
2016-06-29 12:58:14 +0300

    Create new project

---
+++ AKFootball/AKFootball/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKFootball
+//
+//  Created by Admin on 29.06.16.
+//  Copyright © 2016 Karpov Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AKFootball/AKFootball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKFootball
+//
+//  Created by Admin on 29.06.16.
+//  Copyright © 2016 Karpov Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
